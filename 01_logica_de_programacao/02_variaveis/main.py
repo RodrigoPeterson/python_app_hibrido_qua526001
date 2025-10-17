ba6659a8d1f9139174ca0189adf55b99f2aa8b83
RodrigoPeterson <rodrigopetersonfreelas@gmail.com>
2025-10-17 16:40:09 -0300

    Commit da aula 17/10/25. Meu primeiro commit no projeto

---
+++ 01_logica_de_programacao/02_variaveis/main.py
@@ -0,0 +1,11 @@
+# declaração de variáveis 
+nome_completo = "Rodrigo Peterson" # string
+idade = 21 # int
+altura = 1.72 # float
+programador = True # bool (booLean)
+
+# saida de dados
+print(nome_completo)
+print(idade)
+print(altura)
+print(programador)
---
+++ 01_logica_de_programacao/main.py
@@ -0,0 +1,11 @@
+# declarção de variáveis
+nome = "Rodrigo Peterson"
+idade = 21
+altura = 1.72
+programador = True 
+
+# exibe os tipos de dados
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(programador))

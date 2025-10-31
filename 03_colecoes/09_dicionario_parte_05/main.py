891c38147255218ac643701f014c1bce55b44237
RodrigoPeterson <rodrigopetersonfreelas@gmail.com>
2025-10-31 17:29:43 -0300

    Explicação do for. uma lista é uma linha, uma chaves é uma coluna, e as informações dentro das chaves é a informação de dentro de uma célula

---
+++ 03_colecoes/09_dicionario_parte_05/main.py
@@ -0,0 +1,37 @@
+# biblioteca
+import os
+
+#declaração de listas
+usuarios = []
+
+# limpeza de console
+os.system("cls")
+
+while True:
+    # menu
+    print("1 - Cadastrar novo usuário")
+    print("2 - Listar usuários")
+    print("3 - Sair do programa")
+    opcao = input("Informe a opção desejada: ").strip()
+    os.system("cls")
+    match opcao:
+        case "1":
+            usuario = {}
+            usuario['nome'] = input("Informe o nome do usuário: ").strip().title()
+            usuario['idade'] = int(input("Informe a idade do usuário: ").strip())
+            usuario['email'] = (input("Informe o email do usuário: ").strip().lower())
+            usuarios.append(usuario)
+            os.system("cls")
+            print("Novo usuário inserido com sucesso.")
+            continue
+        case "2":
+            for usuario in usuarios:
+                for chave in usuario:
+                    print(f"{chave.capitalize()}: {usuario[chave]}")
+                print(f"{'-'*40}")
+                continue
+        case "3":
+            break
+        case _:
+            print("Opção inválida.")
+            continue

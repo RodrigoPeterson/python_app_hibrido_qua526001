a2634f964ad881d0a189c02d6cb7db7e63679041
RodrigoPeterson <rodrigopetersonfreelas@gmail.com>
2025-12-01 17:32:30 -0300

    Programa para baixar vídeos

---
+++ 07_app_hibrido/05_yt_dowloader/src/main.py
@@ -0,0 +1,131 @@
+import flet as ft
+from pytubefix import Youtube
+
+import os
+import threading
+
+
+def main(page: ft.Page):
+    page.title = "Youtube Dowloader"
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.window.icon = "assests/icon.ico"
+
+    # cria as pastas caso não existam
+    caminho_videos = 'videos'
+    caminho_audios = 'audios'
+    os.makedirs(caminho_videos, exist_ok=True)
+    os.makedirs(caminho_audios, exist_ok=True)
+
+    # componentes da interface gráfica
+    titulo = ft.Text("Use uma URL", color=ft.Colors.BLACK, size=20, weight=ft.FontWeight.BOLD)
+    url = ft.TextField(
+        label="Cole a URL do vídeo do Youtube aqui",
+        width=400,
+        border_radius=10
+    )
+
+    base_path = os.path.dirname(__file__)
+    logo_path = os.path.join(base_path, "assets", "youtube.png")
+    logo_cabecalho = ft.Image(src=logo_path, width=300, height=200)
+
+    # componente para mostrar informações do vídeo
+    video_info = ft.Countainer(
+        content=ft.Column([]),
+        visible=False,
+        padding=10,
+        bgcolor=ft.Colors.BLUE_GREY_50,
+        border_radius=10,
+        width=400
+    )
+
+    # barra de progresso
+    progress_bar = ft.ProgressBar(
+        visible=False,
+        width=400,
+        color=ft.Colors.BRUE,
+        bgcolor=ft.Colors.BLUE_GREY_100
+    )
+
+    # texto de status
+    status_text = ft.Text(
+        "",
+        color=ft.Colors.BLACK,
+        size=14,
+        text_align=ft.TextAlign.CENTER
+    )
+
+    # mostra as informações de vídeo na interface
+    def mostrar_info_videos(yt):
+        try:
+            # limpar o container
+            video_info.content.controls.clear()
+
+            # adiciona informações do vídeo
+            video_info.content.controls.extend(
+                [
+                    ft.Text(f"Título: {yt.title}", size=14, weight=ft.FontWeight.BOLD),
+                    ft.Text(f"Canal: {yt.author}", size=12),
+                    ft.Text(f"Duração: {yt.length/60}:{yt.length%60:02d}", size=12),
+                    ft.Text(f"Visualizações: {yt.views:,}", size=12),
+
+                ]
+            )
+
+            video_info.visible = True
+            page.update()
+
+        except Exception as e:
+            status_text.value = f"Erro ao obter informações: {str(e)}."
+            status_text.color = ft.Colors.RED
+            page.update()
+
+    # função para baixar vídeo
+    def baixar_video(e):
+        if not url.value.strip():
+            status_text.value = "Por favor, insira uma URL válida."
+            status_text.color = ft.Colors.ORANGE
+            page.update()
+
+        def donwload_thread():
+            try:
+                # mostrar progresso
+                progress_bar.visible = True
+                status_text.value = "Analisando vídeo..."
+                status_text.color = ft.Colors.BLUE
+                page.update()
+
+                # criar objeto do youtube
+                yt = Youtube(url.value.strip())
+
+                # capturar as informações do vídeo
+                mostrar_info_videos(yt)
+
+                # inicia donwload
+                status_text.value = f"Baixando vídeo: {yt.title}..."
+                page.update()
+
+                # pega a maior resolução possível
+                stream = yt.streams.get_highest_resolution()
+
+                # TODO: fazer if else do stream
+            except Exception as e:
+                progress_bar.visible = False
+                status_text.value = f"Erro {str(e)}."
+                status_text.color = ft.Colors.RED
+                page.update()
+
+        # executa em thread separada para não travar a interface
+        threading.Thread(target=donwload_thread, daemon=True).start()
+
+   
+    page.add(
+        ft.SafeArea(
+            ft.Container(
+                alignment=ft.alignment.center,
+            ),
+            expand=True,
+        )
+    )
+
+
+ft.app(main)
